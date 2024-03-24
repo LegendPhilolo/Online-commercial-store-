44c6873e2144872b7de20feda57d67f12a7e0816
LegendPhilolo <philipbarongo30@gmail.com>
2024-03-24 09:19:16 +0300

    First push of online store

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/** First Slider */
+$('.slider-one')
+.not(".slick-initialized")
+.slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  dots: true,
+  prevArrow: "i.fa-chevron-left",
+  nextArrow: "i.fa-chevron-right"
+})
